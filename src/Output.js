872e4f3f9207bcd5d25b90a5e32f5bad69d81afb
Ali Madooei <madooei@jhu.edu>
2020-11-19 11:22:48 -0500

    Step 8

--- src/Output.js
+++ src/Output.js
@@ -1,4 +1,13 @@
 function Output() {
+  const cycles = [
+    "11:44 PM",
+    "1:14 AM",
+    "2:44 AM",
+    "4:14 AM",
+    "5:44 AM",
+    "7:14 AM",
+  ];
+
   return (
     <div>
       <p>It takes the average human fourteen minutes to fall asleep.</p>
@@ -6,7 +15,12 @@ function Output() {
         If you head to bed right now, you should try to wake up at one of the
         following times:
       </p>
-      <p>11:44 PM or 1:14 AM or 2:44 AM or 4:14 AM or 5:44 AM or 7:14 AM</p>
+      <ul>
+        {cycles.map((cycle, index) => (
+          <li key={index}>{cycle}</li>
+        ))}
+      </ul>
+
       <p>A good night's sleep consists of 5-6 complete sleep cycles.</p>
     </div>
   );
