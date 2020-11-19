5b4ea999a1aed1ee77e29fc4e245b9a84a9d93c3
Ali Madooei <madooei@jhu.edu>
2020-11-19 11:21:34 -0500

    Step 7

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
+import Output from "./Output.js";
+
 function App() {
   return (
     <div>
       <p>If you go to bed NOW, you should wake up at...</p>
       <button>zzz</button>
-      <p>It takes the average human fourteen minutes to fall asleep.</p>
-      <p>
-        If you head to bed right now, you should try to wake up at one of the
-        following times:
-      </p>
-      <p>11:44 PM or 1:14 AM or 2:44 AM or 4:14 AM or 5:44 AM or 7:14 AM</p>
-      <p>A good night's sleep consists of 5-6 complete sleep cycles.</p>
+      <Output />
     </div>
   );
 }
---
+++ src/Output.js
@@ -0,0 +1,15 @@
+function Output() {
+  return (
+    <div>
+      <p>It takes the average human fourteen minutes to fall asleep.</p>
+      <p>
+        If you head to bed right now, you should try to wake up at one of the
+        following times:
+      </p>
+      <p>11:44 PM or 1:14 AM or 2:44 AM or 4:14 AM or 5:44 AM or 7:14 AM</p>
+      <p>A good night's sleep consists of 5-6 complete sleep cycles.</p>
+    </div>
+  );
+}
+
+export default Output;
