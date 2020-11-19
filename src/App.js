931537a7d4a84abe74238c3b021b1d81ccbbf5b6
Ali Madooei <madooei@jhu.edu>
2020-11-19 11:16:38 -0500

    Step 4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import React from "react";
+
 function App() {
-  return <div>Hello React!</div>;
+  const elm = React.createElement("div", null, "Hello React!");
+  console.log(elm);
+  return elm;
 }
 
 export default App;
