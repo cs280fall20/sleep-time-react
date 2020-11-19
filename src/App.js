d262cd52041a299379ff3b69b45c0771b049506b
Ali Madooei <madooei@jhu.edu>
2020-11-19 11:19:23 -0500

    Step 6

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 function App() {
-  return <div>Hello React!</div>;
+  return (
+    <div>
+      <p>If you go to bed NOW, you should wake up at...</p>
+      <button>zzz</button>
+      <p>It takes the average human fourteen minutes to fall asleep.</p>
+      <p>
+        If you head to bed right now, you should try to wake up at one of the
+        following times:
+      </p>
+      <p>11:44 PM or 1:14 AM or 2:44 AM or 4:14 AM or 5:44 AM or 7:14 AM</p>
+      <p>A good night's sleep consists of 5-6 complete sleep cycles.</p>
+    </div>
+  );
 }
 
 export default App;
-
-/*
-import React from "react";
-
-class App extends React.Component {
-  render() {
-    return <div>Hello React!</div>
-  }
-}
-
-export default App;
-*/
